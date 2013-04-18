12fc8ed6f7c1c15c81bb9fb0d4a71c45cd9a6c3a
iocikun.jujon <PAF01143@nifty.ne.jp>
2013-04-18 17:08:55 +0900

    refactoring: File.Binary.PNG.Chunks.CRC

--- src/File/Binary/PNG/Chunks/CRC.hs
+++ src/File/Binary/PNG/Chunks/CRC.hs
@@ -1,34 +1,33 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module File.Binary.PNG.Chunks.CRC (crc, checkCRC) where
+module File.Binary.PNG.Chunks.CRC (checkCRC, crc) where
 
-import Data.Array
-import Data.Bits
-import Data.Word
-import qualified Data.ByteString.Lazy as BSL
-import qualified Data.ByteString.Lazy.Char8()
+import Prelude hiding (reverse)
+import Data.Array (Array, listArray, (!))
+import Data.Word (Word32)
+import Data.Bits ((.&.), xor, shiftR)
+import Data.ByteString.Lazy (ByteString, cons', append, reverse)
+import qualified Data.ByteString.Lazy as BSL (foldl)
+import Data.ByteString.Lazy.Char8 ()
 
 --------------------------------------------------------------------------------
 
-crc :: BSL.ByteString -> BSL.ByteString
-crc = BSL.reverse . word32ToWord8s . xor 0xffffffff . BSL.foldl crc' 0xffffffff
+checkCRC :: ByteString -> ByteString -> Bool
+checkCRC str c = crc (str `append` reverse c) == "\x21\x44\xdf\x1c"
+
+crc :: ByteString -> ByteString
+crc = reverse . word32ToBS . xor 0xffffffff . BSL.foldl crc' 0xffffffff
 	where
-	crc' :: Word32 -> Word8 -> Word32
-	crc' c x = table ! i `xor` shiftR c 8
-		where
-		i = (c `xor` fromIntegral x) .&. 0xff
+	crc' c x = table ! (c .&. 0xff `xor` fromIntegral x) `xor` shiftR c 8
 
 table :: Array Word32 Word32
-table = listArray (0, 255) $ map (\n -> foldl table' n [0 .. 7]) [0 .. 255]
+table = listArray (0, 255) $ map (\n -> iterate crc8bit n !! 8) [0 .. 255]
 
-table' :: Word32 -> Int -> Word32
-table' c _
+crc8bit :: Word32 -> Word32
+crc8bit c
 	| c .&. 1 == 0 = shiftR c 1
-	| otherwise = xor 0xedb88320 $ shiftR c 1
+	| otherwise = 0xedb88320 `xor` shiftR c 1
 
-checkCRC :: BSL.ByteString -> BSL.ByteString -> Bool
-checkCRC str c = crc (str `BSL.append` BSL.reverse c) == "\x21\x44\xdf\x1c"
-
-word32ToWord8s :: Word32 -> BSL.ByteString
-word32ToWord8s 0 = ""
-word32ToWord8s w = fromIntegral (w .&. 0xff) `BSL.cons'` word32ToWord8s (w `shiftR` 8)
+word32ToBS :: Word32 -> ByteString
+word32ToBS 0 = ""
+word32ToBS w = fromIntegral (w .&. 0xff) `cons'` word32ToBS (w `shiftR` 8)
