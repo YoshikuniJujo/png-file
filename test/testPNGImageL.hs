bcecd89eb4d7e985e80b91533084ff0dd5c6fd0a
iocikun.jujon <PAF01143@nifty.ne.jp>
2013-04-23 13:32:00 +0900

    write icc profile file

--- test/testPNGImageL.hs
+++ test/testPNGImageL.hs
@@ -7,6 +7,7 @@ import Prelude hiding (concat)
 import System.Environment (getArgs)
 import File.Binary(readBinaryFile, writeBinaryFile)
 import Data.List
+import qualified Data.ByteString.Lazy as BSL
 
 --------------------------------------------------------------------------------
 
@@ -23,12 +24,16 @@ main = do
 		o = others cs
 		b = body cs
 		trns = fmap (\(ChunktRNS t) -> t) $ find ((== T_tRNS) . typeChunk) o
+		iccp = fmap (\(ChunkiCCP i) -> i) $ find ((== T_iCCP) . typeChunk) o
 		ph = makePNGHeader i p trns
 		pi :: PNGImageL
 		Right pi = bsToPNGImage i p trns b
 
 		(i', Nothing, b') = pngImageToBS pi
+		iccp' = fmap (readIccp . iccp_body) iccp
 	print ph
+	putStrLn $ take 700 (show iccp) ++ "..."
+	maybe (return ()) (BSL.writeFile "iccp") iccp'
 	putStrLn $ take 700 (show pi) ++ "..."
 
 --	let	binary = putChunks $ mkChunks i p o b
